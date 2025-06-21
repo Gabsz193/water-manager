702def49b6a332556fef4a6356766b389dcfe719
Luiz Gabriel <senaaluizgabriel@gmail.com>
2025-06-21 00:32:26 -0400

    Comentada configuração do Firebase e inicialização dos serviços não utilizados.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import { Inter } from "next/font/google";
 import StyledComponentsRegistry from "@/lib/AntdRegistry"; // Importe o Registry
 import 'antd/dist/reset.css'; // Importa o CSS reset do antd
 import "./globals.css";
-import { initializeApp } from "firebase/app"; // Import the functions you need from the SDKs you need
-import { getAnalytics } from "firebase/analytics";
+// import { initializeApp } from "firebase/app"; // Import the functions you need from the SDKs you need
+// import { getAnalytics } from "firebase/analytics";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,21 +18,21 @@ export const metadata: Metadata = {
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-    apiKey: "AIzaSyASmu_gTNRivhbXjllHCsDoIu7soKe49EQ",
-    authDomain: "watermanager-763a0.firebaseapp.com",
-    databaseURL: "https://watermanager-763a0-default-rtdb.firebaseio.com",
-    projectId: "watermanager-763a0",
-    storageBucket: "watermanager-763a0.firebasestorage.app",
-    messagingSenderId: "633366113229",
-    appId: "1:633366113229:web:254e1211ca4cb241cff284",
-    measurementId: "G-9B8GHKR17N"
-};
+// const firebaseConfig = {
+//     apiKey: "AIzaSyASmu_gTNRivhbXjllHCsDoIu7soKe49EQ",
+//     authDomain: "watermanager-763a0.firebaseapp.com",
+//     databaseURL: "https://watermanager-763a0-default-rtdb.firebaseio.com",
+//     projectId: "watermanager-763a0",
+//     storageBucket: "watermanager-763a0.firebasestorage.app",
+//     messagingSenderId: "633366113229",
+//     appId: "1:633366113229:web:254e1211ca4cb241cff284",
+//     measurementId: "G-9B8GHKR17N"
+// };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 // eslint-disable-next-line
-const analytics = getAnalytics(app);
+// const analytics = getAnalytics(app);
 
 export default function RootLayout({
     children,
